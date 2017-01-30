d5746b55c6f63d4c938c6f91a84c7124a823a90f
Melvin Gruschow and Nick Mead <melvin@>
2017-01-30 09:16:23 -0800

    lazy merge to master

--- js/dice.js
+++ js/dice.js
@@ -3,5 +3,32 @@ var Die = function (sides) {
 }
 
 Die.prototype.roll = function () {
-    return Math.floor((Math.random() * this.sides))//simulate roll
+    return Math.floor((Math.random() * this.sides) + 1)//simulate roll
+}
+
+function battleRolls (numOfDice) {
+    var rolls = []
+    for (i = 0; i < numOfDice; i++) {
+        var die = new Die();
+        rolls.push(die.roll());//new roll into array of rolls
+    }
+    return rolls;
+}
+
+function battle (attackDice, defendDice) {  //pass number of dice for each player
+
+    //create sorted array of rolls for attack and defense
+    var attackRolls = battleRolls(attackDice).sort(function(a, b){return b-a});
+    var defendRolls = battleRolls(defendDice).sort(function(a, b){return b-a});
+    var armiesLost = [0, 0];//array of armiesLost
+
+    for (i = 0; i < Math.min(attackRolls.length, defendRolls.length); i++) {
+        if (attackRolls[i] > defendRolls[i]) {
+            armiesLost[1] -= 1;//defense loses one army
+        } else {
+            armiesLost[0] -= 1;//attack loses one army
+        }
+    }
+
+    return armiesLost;
 }
