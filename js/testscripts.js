df37475d82346edb868eb6e439e89e14044f962d
Derek Villars and Sam kirsch <sam@>
2017-01-31 16:55:04 -0800

    try and test the titles

--- js/testscripts.js
+++ js/testscripts.js
@@ -5,6 +5,50 @@ var makeElement = function(element, elementId, elementText, elementClasses, targ
   newElement.innerHTML = elementText;
   $(targetElement).append(newElement);
 }
+
+var countriesFull = [["Northwest Territory", "northwest-territory", "North America", ["alaska", "alberta", "ontario", "greenland"]],
+                ["Alaska", "alaska", "North America", ["northwest-territory", "alberta", "kamchatka"]],
+                ["Greenland", "greenland", "North America", ["northwest-territory", "ontario", "eastern-canada"]],
+                ["Alberta", "alberta", "North America", ["alaska", "northwest-territory", "ontario", "western-united-states"]],
+                ["Ontario", "ontario", "North America", ["alberta", "northwest-territory", "greenland", "eastern-canada", "eastern-united-states", "western-united-states"]],
+                ["Eastern United States", "eastern-united-states", "North America", ["central-america", "western-united-states", "ontario", "eastern-canada"]],
+                ["Central America", "central-america", "North America", ["venezuela", "western-united-states", "eastern-united-states"]],
+                ["Eastern Canada", "eastern-canada", "North America", ["greenland", "ontario", "eastern-united-states"]],
+                ["Western United States", "western-united-states", "North America", ["alberta", "ontario", "eastern-united-states", "central-america"]],
+                ["Venezuela", "venezuela", "South America", ["central-america", "peru", "brazil"]],
+                ["Peru", "peru", "South America", ["venezuela", "brazil", "argentina"]],
+                ["Argentina", "argentina", "South America", ["peru", "brazil"]],
+                ["Brazil", "brazil", "South America", ["argentina", "peru", "venezuela", "north-africa"]],
+                ["North Africa", "north-africa", "Africa", ["brazil", "western-europe", "southern-europe", "egypt", "east-africa", "central-africa"]],
+                ["Egypt", "egypt", "Africa", ["north-africa", "east-africa", "middle-east", "southern-europe"]],
+                ["East Africa", "east-africa", "Africa", ["madagascar", "south-africa", "central-africa", "north-africa", "egypt", "middle-east"]],
+                ["Madagascar", "madagascar", "Africa", ["south-africa", "east-africa"]],
+                ["South Africa", "south-africa", "Africa", ["madagascar", "central-africa", "east-africa"]],
+                ["Central Africa", "central-africa", "Africa", ["south-africa", "north-africa", "east-africa"]],
+                ["Western Europe", "western-europe", "Europe", ["north-africa", "great-britain", "northern-europe", "southern-europe"]],
+                ["Great Britain", "great-britain", "Europe", ["iceland", "scandanavia", "northern-europe", "western-europe"]],
+                ["Iceland", "iceland", "Europe", ["greenland", "scandanavia", "great-britain"]],
+                ["Scandanavia", "scandanavia", "Europe", ["russia", "northern-europe", "great-britain", "iceland"]],
+                ["Ukraine", "ukraine", "Europe", ["ural", "afghanistan", "middle-east", "southern-europe", "northern-europe", "scandanavia"]],
+                ["Southern Europe", "southern-europe", "Europe", ["middle-east", "egypt", "western-europe", "northern-europe", "russia", "northern-africa"]],
+                ["Northern Europe", "northern-europe", "Europe", ["southern-europe", "western-europe", "great-britain", "scandanavia", "russia"]],
+                ["Middle East", "middle-east", "Asia", ["east-africa", "egypt", "southern-europe", "russia", "afghanistan", "india"]],
+                ["India", "india", "Asia", ["middle-east", "afghanistan", "china", "southeast-asia"]],
+                ["Southeast Asia", "southeast-asia", "Asia", ["india", "china", "indonesia"]],
+                ["China", "china", "Asia", ["india", "southeast-asia", "afghanistan", "ural", "siberia", "mongolia"]],
+                ["Afghanistan", "afghanistan", "Asia", ["russia", "middle-east", "india", "china", "ural"]],
+                ["Ural", "ural", "Asia", ["russia", "afghanistan", "china", "siberia"]],
+                ["Siberia", "siberia", "Asia", ["yakutsk", "irkutsk", "mongolia", "china", "ural"]],
+                ["Yakutsk", "yakutsk", "Asia", ["siberia", "irkutsk", "kamchatka"]],
+                ["Irkutsk", "irkutsk", "Asia", ["kamchatka", "yakutsk", "siberia", "mongolia"]],
+                ["Kamchatka", "kamchatka", "Asia", ["japan", "mongolia", "alaska", "irkutsk", "yakutsk"]],
+                ["Mongolia", "mongolia", "Asia", ["siberia", "china", "japan", "kamchatka", "irkutsk"]],
+                ["Japan", "japan", "Asia", ["kamchatka", "mongolia"]],
+                ["Indonesia", "indonesia", "Australia", ["new-guinea", "southeast-asia", "western-australia"]],
+                ["New Guinea", "new-guinea", "Australia", ["eastern-australia", "western-australia", "indonesia"]],
+                ["Eastern Austalia", "eastern-australia", "Australia", ["new-guinea", "western-australia"]],
+                ["Western Australia", "western-australia", "Australia", ["indonesia", "eastern-australia", "new-guinea"]]];
+
 var locations = [["nw-territory", 500, 420],
                 ["alberta", 431, 486],
                 ["ontario", 488, 461],
@@ -78,6 +122,12 @@ var placeIcon = function(coordsId) {
 
 $(document).ready(function() {
   $('.clickable-space').click(function() {
-    placeIcon($(this).attr('id'));
+    for (var index = 0; index < countriesFull.length; index++) {
+      var inIt = false;
+      if ($(this).attr('id') === countriesFull[index][1]) {
+        console.log(countriesFull[index][1] + " in it. Equal to " + $(this).attr('id'));
+        console.log(index);
+      }
+    }
   });
 });
