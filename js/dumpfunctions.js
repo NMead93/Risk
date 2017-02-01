ac701158d790920ba976c403a3b1edee8849b7bc
Derek Villars and Sam Kirsch <sam@>
2017-02-01 12:59:46 -0800

    move utility scripts to the dumpscripts.js file and clear out testscripts.js to start testing a pathfinding function for contiguous movement

--- js/dumpfunctions.js
+++ js/dumpfunctions.js
@@ -4,3 +4,28 @@ var generateElements = function(arrayOfCountries) {
     makeElement('span', '', " Troops ", "peeps", "#" +  arrayOfCountries[index].countryId);
   }
 }
+
+var createIdOnClick = function(event) {
+  // utility script to determine the coordinates of a click
+  counter+=1
+  console.log(counter);
+  var coordsId = $(this).attr('id');
+  makeElement('div', coordsId, coordsId + ": (" + event.pageX + ", " + event.pageY + ")", "soldier-coordinates", ".target-holder");
+  console.log("(" + event.pageX + ", " + event.pageY + ")");
+}
+
+var compareLists = function(array1, array2) {
+  // utility script to check if elements in an array are present in another
+  for (var index = 0; index < array1.length; index++) {
+    var contains = false;
+    for (var jdex = 0; jdex < array2.length; jdex++) {
+      if (array2[jdex][0] === array1[index][1]) {
+        contains = true;
+        break;
+      }
+    }
+    if (contains === false) {
+      console.log(array1[index][1]);
+    }
+  }
+}
